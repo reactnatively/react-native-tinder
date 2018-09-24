7b1bd426000445ad3f540979afeb69cb88bc2eac
Qappture <qappture@gmail.com>
2018-09-24 09:04:13 -0500

    THIS IS THE REAL Lecture 5 - the other was lecture 4 - facebook login works

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Expo from 'expo';
 import styles from '../styles';
 import { login } from '../redux/actions';
 
 import {
   Text,
-  View
+  View,
+  Alert,
 } from 'react-native';
 
 class Home extends React.Component {
   state = {}
 
+  //
   componentWillMount() {
     this.props.dispatch(login("what's appp"))
+    this.login()
+  }
+
+  //
+  login = async () => {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('2162779303732501', {
+        permissions: ['public_profile'],
+      });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert(
+        'Logged in!',
+        `Hi ${(await response.json()).name}!`,
+      );
+    }
   }
 
   render() {
