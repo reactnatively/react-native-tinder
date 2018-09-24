719e747e8cb46379c1466f413cc99f4dbeb11fca
Qappture <qappture@gmail.com>
2018-09-24 06:44:51 -0500

    Lecture 3

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import Matches from '../screens/Matches';
+
+export default TabNavigator(
+  {
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: 'Profile',
+      },
+    },
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+      }
+    },
+    Things: {
+      screen: Matches,
+      navigationOptions: {
+        tabBarLabel: 'Matches',
+      },
+    },
+    Matches: {
+      screen: Matches,
+      navigationOptions: {
+        tabBarLabel: 'Matches',
+      },
+    },
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarPosition: 'top',
+    initialRouteName: 'Home',
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarOptions: {
+      style: {
+        height: 100,
+        backgroundColor: '#fff',
+      },
+    }
+  }
+);
